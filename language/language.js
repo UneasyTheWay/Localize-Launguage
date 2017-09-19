ea405f855d264b4315b42c76875f1580f74a3908
HAO <31822499+UneasyTheWay@users.noreply.github.com>
2017-09-19 15:55:47 +0800

    Create a UneasyTheWay

---
+++ language/language.js
@@ -0,0 +1,16 @@
+﻿var testLanguage = {
+    "EN": {
+        "href": "href",
+        "title": "Localize Language",
+        "instructions": "Implementing internationalized languages through JS",
+        "message": "author:Uneasy The Way<br/>Git:https://github.com/UneasyTheWay",
+        "input": "input"
+    },
+    "CN": {
+        "href": "标签",
+        "title": "本地化语言",
+        "instructions": "通过js实现国际化语言",
+        "message": "作者：不安的路<br/>Git:https://github.com/UneasyTheWay",
+        "input": "输入"
+    }
+}
